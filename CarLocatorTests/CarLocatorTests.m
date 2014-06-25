e132e3b721989733f8f1cfb32d6da576acde6d7c
PPTS <ppts@administrators-Mac.local>
2014-06-25 13:56:55 +0530

    Initial Commit

---
+++ CarLocatorTests/CarLocatorTests.m
@@ -0,0 +1,34 @@
+//
+//  CarLocatorTests.m
+//  CarLocatorTests
+//
+//  Created by PPTS on 25/06/14.
+//  Copyright (c) 2014 ppts. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CarLocatorTests : XCTestCase
+
+@end
+
+@implementation CarLocatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
